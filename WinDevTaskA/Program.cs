60d138240348e1c8dd1e685fa17eae6500a699d2
Johannes Håkansson <johhki-4@student.ltu.se>
2021-04-13 13:38:24 +0200

    Initialization of repo

---
+++ WinDevTaskA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace WinDevTaskA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String data_path = Directory.GetCurrentDirectory() + "\\testdata1.txt";
+
+            String[] data = File.ReadAllLines(data_path);
+
+            foreach (String line in data) {
+                Console.WriteLine("line: " + line);
+            }
+        }
+    }
+}
