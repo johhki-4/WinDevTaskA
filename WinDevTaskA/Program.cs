9930028eae18997b3aa4607fb0252284c1d9e84b
Johannes Håkansson <johhki-4@student.ltu.se>
2021-04-13 14:46:22 +0200

    basic structure of UPC-A

--- WinDevTaskA/Program.cs
+++ WinDevTaskA/Program.cs
@@ -11,8 +11,26 @@ namespace WinDevTaskA
             String[] data = File.ReadAllLines(data_path);
 
             foreach (String line in data) {
+                line.Trim();
                 Console.WriteLine("line: " + line);
+                Extract_valid_data(line);
+                foreach (Char ch in line) {
+                    Console.WriteLine(ch);
+                }
+
             }
+        }
+
+        //Left hand is (2,7) (2+7, 7) (2+7+7, 7) (2+7+7+7, 7) etc..
+        static String Extract_valid_data(String line)
+        {
+            String left_guard = line.Substring(0, 3);
+            String[] left_hand;
+            String center_guard = line.Substring(44, 5);
+            String[] right_hand;
+            String right_guard = line.Substring(line.Length-3, 3);
+            Console.WriteLine("line start:" + left_guard + "-" + center_guard + "-" + right_guard + ".");
+            return left_guard;
         }
     }
 }
