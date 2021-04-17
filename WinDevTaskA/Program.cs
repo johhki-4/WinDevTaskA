7df40b3b89c3ccf9b83f8ca66ff44426c6502003
Johannes Håkansson <johhki-4@student.ltu.se>
2021-04-17 10:55:58 +0200

    added encoding tables and methods to extract left/right data

--- WinDevTaskA/Program.cs
+++ WinDevTaskA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace WinDevTaskA
@@ -9,28 +10,95 @@ namespace WinDevTaskA
         {
             String data_path = Directory.GetCurrentDirectory() + "\\testdata1.txt";
             String[] data = File.ReadAllLines(data_path);
+            var left_hand = new List<String>();
+            var right_hand = new List<String>();
+            var encoding_table_left = new Dictionary<String, String>()
+            {
+                {"0", "   || |"},
+                {"1", "  ||  |"},
+                {"2", "  |  ||"},
+                {"3", " |||| |"},
+                {"4", " |   ||"},
+                {"5", " ||   |"},
+                {"6", " | ||||"},
+                {"7", " ||| ||"},
+                {"8", " || |||"},
+                {"9", "   | ||"}
+            };
+            var encoding_table_right = new Dictionary<String, String>()
+            {
+                {"0", "|||  | "},
+                {"1", "||  || "},
+                {"2", "|| ||  "},
+                {"3", "|    | "},
+                {"4", "| |||  "},
+                {"5", "|  ||| "},
+                {"6", "| |    "},
+                {"7", "|   |  "},
+                {"8", "|  |   "},
+                {"9", "||| |  "}
+            };
 
             foreach (String line in data) {
                 line.Trim();
-                Console.WriteLine("line: " + line);
-                Extract_valid_data(line);
-                foreach (Char ch in line) {
-                    Console.WriteLine(ch);
+                
+                left_hand = ExtractLeftData(line);
+                Console.WriteLine("left:");
+                foreach (String code in left_hand)
+                {
+                    Console.WriteLine(code);
+                    //TODO Send to function that translates each code to numeric
+                    TranslateToNumeric(code);
+                }
+                Console.WriteLine();
+
+                right_hand = ExtractRightData(line);
+                Console.WriteLine("Right:");
+                foreach (String code in right_hand)
+                {
+                    Console.WriteLine(code);
                 }
 
             }
         }
 
-        //Left hand is (2,7) (2+7, 7) (2+7+7, 7) (2+7+7+7, 7) etc..
-        static String Extract_valid_data(String line)
+        //Left hand is (3,7) (3+7, 7) (3+7+7, 7) (3+7+7+7, 7) etc..
+        static List<String> ExtractLeftData(String line)
         {
-            String left_guard = line.Substring(0, 3);
-            String[] left_hand;
-            String center_guard = line.Substring(44, 5);
-            String[] right_hand;
-            String right_guard = line.Substring(line.Length-3, 3);
-            Console.WriteLine("line start:" + left_guard + "-" + center_guard + "-" + right_guard + ".");
-            return left_guard;
+            //String left_guard = line.Substring(0, 3);
+            var left_data = new List<String>();
+            for (int i = 0; i < 6; i++)
+            {
+                left_data.Add(line.Substring(3 + (7 * i), 7));
+            }
+            //String center_guard = line.Substring(44, 5);
+            //var right_hand = new List<String>();
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    right_hand.Add(line.Substring(50 + (7 * i), 7));
+            //}
+            //String right_guard = line.Substring(line.Length - 3, 3);
+            //Console.WriteLine("line start:" + left_guard + "-" + center_guard + "-" + right_guard + ".");
+            return left_data;
+        }
+
+        static List<String> ExtractRightData(String line)
+        {
+            var right_data = new List<String>();
+            for (int i = 0; i < 6; i++)
+            {
+                right_data.Add(line.Substring(50 + (7 * i), 7));
+            }
+            return right_data;
+        }
+
+        static String TranslateToNumeric(String code)
+        {
+            
+
+
+            String numeric = code;
+            return numeric;
         }
     }
 }
